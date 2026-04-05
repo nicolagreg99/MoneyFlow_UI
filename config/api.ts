db27f757fd623bc1ee39aa1de1322917043c65f6
Nicola <nicola.greggio@inkwelldata.com>
2026-04-05 11:04:04 +0200

    Change dns

--- config/api.ts
+++ config/api.ts
@@ -1,5 +1,6 @@
 // config/api.ts
-const BASE_URL = "https://backend.money-app-api.com";
+const BASE_URL = "https://ec2-13-51-246-167.eu-north-1.compute.amazonaws.com";
+// const BASE_URL = "http://192.168.1.10:5000";
 
 export default {
   BASE_URL,
